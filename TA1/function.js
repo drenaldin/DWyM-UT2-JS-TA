ccb7ff95a82fb0d6e177d7ab8b633812b0ef2685
Diego Renaldin <diego.renaldin@hotmail.com>
2025-03-27 18:37:21 -0300

    TA 1 Y TA 2

---
+++ TA1/function.js
@@ -0,0 +1,15 @@
+// <!-- TA 1
+// Dado un archivo HTML, crear un archivo .js que implemente una función llamada repeatString
+//  (texto: string, repeticiones:int), que utilizando un loop, imprima el páramentro texto tantas veces como el parámetro repeticiones indique.
+// La impresión puede hacerse usando console.log(texto) -->
+
+function repeatString(texto, repeticiones) {
+    for (let i = 0; i < repeticiones; i++) {
+        console.log(texto);
+    }
+}
+
+
+repeatString("Hola mundo", 3);
+
+      
